96c6076a62ed75322ffbab35242f84d0ab0b1ea5
Bohdan <bohdanvoroniak455@gmail.com>
2023-08-22 23:12:05 +0300

    add sorting service

--- BLL/Service/Realizations/SortingService.cs
+++ BLL/Service/Realizations/SortingService.cs
@@ -20,21 +20,34 @@ namespace BLL.Service.Realizations
             _unitOfWork = unitOfWork;
         }
 
-        public async Task<IEnumerable<Author>> SortAuthorsByPopularity(string userId)
+        public async Task<IEnumerable<Author?>> SortAuthorsByPopularity(string userId)
         {
-
+            //TODO Check if this causes null error
             var result = await _unitOfWork.Author.GetAllAsync(a => a.UserId == userId,ar => ar.Include(a => a.Books).ThenInclude(b => b.OrderParts));
-                
+            return result.OrderByDescending(a => a?.Books.Sum(b => b.OrderParts.Sum(op => op.Quantity)));
         }
 
-        public Task<IEnumerable<Genre>> SortGenresByPopularity(string userId)
+        public async Task<IEnumerable<Genre?>> SortGenresByPopularity(string userId)
         {
-            throw new NotImplementedException();
+            //TODO Check if this causes null error
+            var result = await _unitOfWork.Genre.GetAllAsync(g => g.UserId == userId, gr => gr.Include(g => g.Books).ThenInclude(g => g.OrderParts));
+            return result.OrderByDescending(g => g?.Books.Sum(b => b.OrderParts.Sum(op => op.Quantity)));
         }
 
-        public Task<IEnumerable<BookBriefInformation>> SortBooksByPopularity(string userId)
+        public async Task<IEnumerable<BookBriefInformation?>> SortBooksByPopularity(string userId)
         {
-            throw new NotImplementedException();
+            var result = await _unitOfWork.Book.GetAllAsync(b => b.UserId == userId, 
+                br => br.Include(b => b.OrderParts).Include(b => b.Author).Include(b => b.Genre));
+
+            return result.OrderBy(b => b.OrderParts.Sum(od => od.Quantity)).Select(b => new BookBriefInformation
+            {
+                Author = b.Author.Name,
+                Genre = b.Genre.Name,
+                Id = b.Id,
+                SellingPrice = b.SellingPrice,
+                Title = b.Title,
+                Year = b.Year,
+            });
         }
     }
 }
